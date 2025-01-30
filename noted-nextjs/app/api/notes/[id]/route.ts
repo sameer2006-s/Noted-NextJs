490aa39a72454483da854e632c8f075048a9fd7c
sameer2006-s <sameer.ahmed20062006@gmail.com>
2025-01-30 16:33:48 +0200

    fix

--- noted-nextjs/app/api/notes/[id]/route.ts
+++ noted-nextjs/app/api/notes/[id]/route.ts
@@ -2,36 +2,10 @@ import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase client initialization
-const supabaseUrl = 'https://gbjtqylynbmhrxietpxt.supabase.co';
+const supabaseUrl = process.env.SUPABASE_URL || '';
 const supabaseKey = process.env.SUPABASE_KEY || '';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Handle GET request for a specific note
-// export async function GET(req: Request, { params }: { params: { id: string } }) {
-//   const {id} =  await params;
-
-//   if (!id) {
-//     return NextResponse.json({ message: 'Note ID is required' }, { status: 400 });
-//   }
-
-//   try {
-//     const { data, error } = await supabase.from('notes').select('*').eq('id', id).single();
-
-//     if (error) {
-//       return NextResponse.json({ message: 'Error fetching note', error }, { status: 500 });
-//     }
-
-//     if (!data) {
-//       return NextResponse.json({ message: 'Note not found' }, { status: 404 });
-//     }
-
-//     return NextResponse.json(data, { status: 200 });
-//   } catch (err) {
-//     return NextResponse.json({ message: 'Unexpected error', error: err }, { status: 500 });
-//   }
-// }
-
-// Handle PUT request to update a specific note
 export async function PUT(req: Request, context: { params:any }) {
   const id =  context.params.id;
 
@@ -56,9 +30,6 @@ export async function PUT(req: Request, context: { params:any }) {
       return NextResponse.json({ message: 'Error updating note', error }, { status: 500 });
     }
 
-    // if (!data ) {
-    //   return NextResponse.json({ message: 'Note not found' }, { status: 404 });
-    // }
 
     return NextResponse.json({ message: 'Note updated successfully', data }, { status: 200 });
   } catch (err) {
@@ -80,10 +51,6 @@ export async function DELETE(req: Request, { params }: { params: any }) {
     if (error) {
       return NextResponse.json({ message: 'Error deleting note', error }, { status: 500 });
     }
-
-    // if (!data ) {
-    //   return NextResponse.json({ message: 'Note not found' }, { status: 404 });
-    // }
 
     return NextResponse.json({ message: 'Note deleted successfully', data }, { status: 200 });
   } catch (err) {
